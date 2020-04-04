62c998a708db8b1712b1bf84a8a99818771c41d7
Navxxx <brochuterence@gmail.com>
2020-04-04 11:23:48 +0200

    change db

--- Model/dbconnexion.php
+++ Model/dbconnexion.php
@@ -2,5 +2,18 @@
 <?php
 
 include(__DIR__."/../admin/access_dp.php");
-$db = new PDO('mysql:host='.HOST_DB.';dbname='.USE_DB.';charset=utf8', USER_DB, MDP_DB);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
+// $db = new PDO('mysql:host='.HOST_DB.';dbname='.USE_DB.';charset=utf8', USER_DB, MDP_DB);
+// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
+
+
+/* Connexion à une base MySQL avec l'invocation de pilote */
+$dsn = 'mysql:dbname='.USE_DB.';host='.HOST_DB;
+$user = USER_DB;
+$password = MDP_DB;
+
+try {
+    $db = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
+
