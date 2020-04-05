d6c4afe7121867311c4fb08501391ef3acb2c8fc
Navxxx <brochuterence@gmail.com>
2020-04-05 04:30:15 +0200

    esretour

--- Model/Requests/postclearvisibility.php
+++ Model/Requests/postclearvisibility.php
@@ -2,51 +2,18 @@
 
 require('../getRequest.php'); // header, autoload, and DB connexion
 
-// $card_post = (isset($_POST['card_post'])) ? intval($_POST['card_post']) : NULL;
+$card_post = (isset($_POST['card_post'])) ? intval($_POST['card_post']) : NULL;
 
 $cardsmanager = new CardsManager($db); // Creation of spacemen manager
 $cards = $cardsmanager->getList();
 
-$newSort = [];
 
 foreach ($cards as $card) {
 
     $card->clearvisibility();
-    // $cardsmanager->update($card);
+    $cardsmanager->update($card);
 
-    array_push($newSort, $card->sort());
-
-}
-    // $player->bet($bet_post);
-    // $playersmanager->update($player);
-    shuffle($newSort);
-    // echo json_encode($newSort);
-
-    $i =0;
-    foreach ($cards as $card) {
-
-
-      $card->setSort($newSort[$i]);
-      $cardsmanager->update($card);
-      $i++;
-    } 
-$playersmanager = new PlayersManager($db); // Creation of spacemen manager
-
-// echo $player_post;
-$players = $playersmanager->getList();
-
-
-if (count($players) > 1) {
-
- 
-  foreach ($players as $player) {
-      $player->actionunfold();
-      $player->bet(0);
-      $playersmanager->update($player);
-  } 
-
-
-}
+} 
 
 require('../Session/sessionDown.php'); // database connexion
 
