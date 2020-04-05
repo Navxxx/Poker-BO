f86b5fd9dcf997236817b83617fd20756969f0eb
Navxxx <brochuterence@gmail.com>
2020-04-05 04:02:01 +0200

    shuff

--- Model/Requests/postclearvisibility.php
+++ Model/Requests/postclearvisibility.php
@@ -7,13 +7,33 @@ $card_post = (isset($_POST['card_post'])) ? intval($_POST['card_post']) : NULL;
 $cardsmanager = new CardsManager($db); // Creation of spacemen manager
 $cards = $cardsmanager->getList();
 
+$newSort = [];
 
 foreach ($cards as $card) {
 
     $card->clearvisibility();
     $cardsmanager->update($card);
+    array_push($newSort, $card->sort());
 
 } 
+
+$playersmanager = new PlayersManager($db); // Creation of spacemen manager
+
+// echo $player_post;
+$players = $playersmanager->getList();
+
+
+if (count($players) > 1) {
+
+ 
+  foreach ($players as $player) {
+      $player->actionunfold();
+      $player->bet(0);
+      $playersmanager->update($player);
+  } 
+
+
+}
 
 require('../Session/sessionDown.php'); // database connexion
 
