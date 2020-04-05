c26cc2495738e06202a4107844e2374f98b129a8
Navxxx <brochuterence@gmail.com>
2020-04-05 04:35:04 +0200

    d

--- Model/Requests/postclearvisibility.php
+++ Model/Requests/postclearvisibility.php
@@ -16,5 +16,51 @@ foreach ($cards as $card) {
 
 } 
 
+
+
+$cards = $cardsmanager->getList();
+
+$newSort = [];
+
+foreach ($cards as $card) {
+
+
+  array_push($newSort, $card->sort());
+  // $cardsmanager->update($card);
+
+} 
+
+  // $player->bet($bet_post);
+  // $playersmanager->update($player);
+  shuffle($newSort);
+  // echo json_encode($newSort);
+
+  $i =0;
+  foreach ($cards as $card) {
+
+
+    $card->setSort($newSort[$i]);
+    $cardsmanager->update($card);
+    $i++;
+
+  } 
+  $playersmanager = new PlayersManager($db); // Creation of spacemen manager
+
+  // echo $player_post;
+  $players = $playersmanager->getList();
+  
+  
+  if (count($players) > 1) {
+  
+   
+    foreach ($players as $player) {
+        $player->actionunfold();
+        $player->bet(0);
+        $playersmanager->update($player);
+    } 
+  
+  
+  }
+
 require('../Session/sessionDown.php'); // database connexion
 
