0ef325f91b39d0174a800c8e190c4dae38e94892
Navxxx <brochuterence@gmail.com>
2020-04-05 04:32:12 +0200

    retou

--- Model/Requests/postclearvisibility.php
+++ Model/Requests/postclearvisibility.php
@@ -1,6 +1,7 @@
 <?php
 
 require('../getRequest.php'); // header, autoload, and DB connexion
+header("Access-Control-Allow-Origin: *");
 
 $card_post = (isset($_POST['card_post'])) ? intval($_POST['card_post']) : NULL;
 
